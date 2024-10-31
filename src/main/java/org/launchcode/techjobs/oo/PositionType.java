9f74faf9a374624a54ec618cbc9424beffee6288
StaceyO21 <staceyobrien21@gmail.com>
2024-10-30 20:45:32 -0500

    GA2 task 2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,19 +1,52 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
     private static int nextId = 1;
     private String value;
 
+
     public PositionType() {
         id = nextId;
         nextId++;
     }
 
-    public PositionType(String value) {
+    public PositionType(String aValue) {
         this();
-        this.value = value;
+        this.value = aValue;
+    }
+
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+
+        PositionType that = (PositionType) o;
+        return this.id == that.id
+                && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
@@ -35,4 +68,13 @@ public class PositionType {
         this.value = value;
     }
 
+    public static void main(String[] args) {
+    PositionType position1 = new PositionType("Manager");
+    PositionType position2 = new PositionType("Director");
+    PositionType position3 = new PositionType("Manager");
+
+    // Test equals method
+        System.out.println(position1.equals(position1));
+        System.out.println(position1.equals(position3));
+}
 }
